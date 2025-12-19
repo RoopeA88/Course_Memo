570a9f4b9728da99d5f9da4d3ebd5a8e0e91f46e
roope <roope.aalto88@gmail.com>
2025-12-19 14:46:40 +0200

    suunniteltu layouttia

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import Header from "./Header"
+
+function App() {
+  
+
+  return (
+    <>
+      <div id="mainDiv">
+        <div id="mainProgramDiv">
+          <Header/>
+        </div>
+        <div id="programNameMainDiv">
+
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Header.jsx
@@ -0,0 +1,19 @@
+function Header(){
+    return(
+        <div id="mainHeaderDiv">
+            <div id="mainSessionDiv">
+                <div id="sessionTextDiv">
+                    <h3>SESSION STATUS:</h3>
+                </div>
+                <div id="onOffTextDiv">
+                    <h3>ACTIVE/CLOSED</h3>
+                </div>
+            </div>
+            <div id="startSessionDiv">
+                <button id="startSessionButton"></button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
